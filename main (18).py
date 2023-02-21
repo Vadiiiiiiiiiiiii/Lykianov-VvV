424d5f0bebe3db7cba934d02f12422f064482221
Vadiiiiiiiiiiiii <124046382+Vadiiiiiiiiiiiii@users.noreply.github.com>
2023-02-21 13:16:59 +0300

    domashka 3

---
+++ main (18).py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+c = 0
+if a > b: c = a-b
+elif a == b: c = a
+else: c = a + b
+print(c)
---
+++ main (19).py
@@ -0,0 +1,12 @@
+a = input ('a = ')
+b = input ('b = ')
+if a > b:
+  c = a - b
+  print ('c')
+elif a < b:
+  c = a + b
+  print ('c')
+else:
+  c = a
+  print ('c')
+input()
