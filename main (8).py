219bf50622a990e2b93a38ad76a744800359d6bc
Vadiiiiiiiiiiiii <124046382+Vadiiiiiiiiiiiii@users.noreply.github.com>
2023-03-07 14:23:22 +0300

    07.03.2023

--- main (8).py
+++ main (8).py
@@ -1,16 +1,7 @@
-tseloe=10 
-drobnoe=8.4 
-stroka='No' 
-stroka2='Yes' 
-stroka3='_' 
-big_netseloe= tseloe * 3.5 
-drobnoe=drobnoe - 1 
-stroka=stroka * 2  
-stroka2 = stroka2 * 3  
-stroka = stroka + stroka3 + stroka2 
-print (tseloe/ drobnoe) 
-print (big_netseloe/drobnoe) 
-print (tseloe) 
-print (drobnoe) 
-print (stroka) 
-print (big_netseloe)
+stsquare = int(input()) 
+radius = int(input()) 
+diametr = radius * 2 
+if stsquare >= diametr: 
+ print('Впишется') 
+else: 
+ print('Не впишется')
